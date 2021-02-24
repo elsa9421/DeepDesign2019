40e8e697855f3af65a9cecb1c0ff0881e6a786af
elsa9421 <61336149+elsa9421@users.noreply.github.com>
2021-02-24 16:43:44 -0500

    Remove tab

--- run_2Dto3Dstyletransfer_options.sh
+++ run_2Dto3Dstyletransfer_options.sh
@@ -7,11 +7,11 @@ CONTENT_WEIGHT=$6
 NUM_ITERS=$7
 LR_VERTICES=$8
 LR_TEXTURES=$9
-TEXTURE_SIZE=$10
-IMAGE_SIZE=$11
-
+TEXTURE_SIZE=${10}
+IMAGE_SIZE=${11}
 
 #cd 2D_3D_style_dream_neural_renderer/
+
 cd 2d_3d_style_dream_neural_renderer
 
 python ./run_examples_style_transfer_3d/run.py \
@@ -21,7 +21,7 @@ python ./run_examples_style_transfer_3d/run.py \
     --lambda_style ${STYLE_WEIGHT} \
     --lambda_content ${CONTENT_WEIGHT} \
     --num_iteration ${NUM_ITERS} \
-	--lr_vertices ${LR_VERTICES} \
-	--lr_textures ${LR_TEXTURES} \
-	--texture_size ${TEXTURE_SIZE} \
-	--image_size ${IMAGE_SIZE} 
+    --lr_vertices ${LR_VERTICES} \
+    --lr_textures ${LR_TEXTURES} \
+    --texture_size ${TEXTURE_SIZE} \
+    --image_size ${IMAGE_SIZE} 
